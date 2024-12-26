7f92d234e12d5546a102253609be46217b1d2cff
Komal Kaur <komalkaler849@gmail.com>
2024-12-25 23:07:25 -0600

    updated logo

--- new_nasa/src/App.jsx
+++ new_nasa/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import nasaLogo from "./assets/nasa-6.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
@@ -12,8 +12,8 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.nasa.gov/" target="_blank">
+          <img src={nasaLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>API integration</h1>
